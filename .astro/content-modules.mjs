21b9e1e2b18a1814f0bf85cf8c4112684e158169
Karin Huber <karin@software-architects.at>
2025-02-23 11:06:11 +0100

    Update text

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/pages/ordination.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fordination.mdx&astroContentModuleFlag=true")],
+["src/content/pages/kontakt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fkontakt.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fhome.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/impressum.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fimpressum.mdx&astroContentModuleFlag=true")],
-["src/content/pages/kontakt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fkontakt.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/ordination.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fordination.mdx&astroContentModuleFlag=true")]]);
 		
