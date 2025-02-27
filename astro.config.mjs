74996c53aae4f98082e0879643a813101004b0bc
Karin Huber <karin@software-architects.at>
2025-02-27 07:02:08 +0100

    Remove target-blank plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { targetBlank } from './src/plugins/target-blank';
+//import { targetBlank } from './src/plugins/target-blank';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.ordination-weissenwolffstrasse.at',
   markdown: {
     rehypePlugins: [
-      [targetBlank, { domain: 'www.ordination-weissenwolffstrasse.at' }],
+      //[targetBlank, { domain: 'www.ordination-weissenwolffstrasse.at' }],
     ],
   },
   integrations: [mdx(), sitemap()],
