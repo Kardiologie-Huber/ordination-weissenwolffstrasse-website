38e18ff3045779c391697668594b4578fc7a75d2
Karin Huber <karin@software-architects.at>
2025-09-21 11:49:15 +0200

    Ordinationsräume

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,7 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z1YNTFx from "src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx";
+import __ASTRO_IMAGE_IMPORT_GgI82 from "src/assets/images/empfang-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fhome.mdx";
+import __ASTRO_IMAGE_IMPORT_1lnYt8 from "src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fkontakt.mdx";
+import __ASTRO_IMAGE_IMPORT_Z1sibuo from "src/assets/images/wartebereich-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx";
+export default new Map([["src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx", __ASTRO_IMAGE_IMPORT_Z1YNTFx], ["src/assets/images/empfang-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fhome.mdx", __ASTRO_IMAGE_IMPORT_GgI82], ["src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fkontakt.mdx", __ASTRO_IMAGE_IMPORT_1lnYt8], ["src/assets/images/wartebereich-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx", __ASTRO_IMAGE_IMPORT_Z1sibuo]]);
+		
--- src/content/config.ts
+++ src/content/config.ts
@@ -5,15 +5,18 @@ import { defineCollection, z } from 'astro:content';
 const pagesCollection = defineCollection({
   type: 'content', // or 'data' if you're storing data files
   // Optional: Add schema for content validation
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    slug: z.string().optional(),
-    heroimage: z.string().optional(),
-    herotitle: z.string().optional(),
-    herodescription: z.string().optional(),
-    // Add other fields as needed
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      slug: z.string().optional(),
+      heroimage: image().optional(),
+      heroimagedarkness: z.number().optional(),
+      heroimageposition: z.string().optional(),
+      herotitle: z.string().optional(),
+      herodescription: z.string().optional(),
+      // Add other fields as needed
+    }),
 });
 
 // Export the collections object
