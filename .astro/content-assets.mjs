2778f29d91bb68b2142b2b96e1dc2e34d0dbb029
Karin Huber <karin@software-architects.at>
2025-09-22 20:08:09 +0200

    Fotos

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1YNTFx from "src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx";
 import __ASTRO_IMAGE_IMPORT_GgI82 from "src/assets/images/empfang-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fhome.mdx";
-import __ASTRO_IMAGE_IMPORT_1lnYt8 from "src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fkontakt.mdx";
 import __ASTRO_IMAGE_IMPORT_Z1sibuo from "src/assets/images/wartebereich-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx";
-export default new Map([["src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx", __ASTRO_IMAGE_IMPORT_Z1YNTFx], ["src/assets/images/empfang-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fhome.mdx", __ASTRO_IMAGE_IMPORT_GgI82], ["src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fkontakt.mdx", __ASTRO_IMAGE_IMPORT_1lnYt8], ["src/assets/images/wartebereich-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx", __ASTRO_IMAGE_IMPORT_Z1sibuo]]);
+import __ASTRO_IMAGE_IMPORT_1lnYt8 from "src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fkontakt.mdx";
+export default new Map([["src/assets/images/empfang-4.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fhome.mdx", __ASTRO_IMAGE_IMPORT_GgI82], ["src/assets/images/wartebereich-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx", __ASTRO_IMAGE_IMPORT_Z1sibuo], ["src/assets/images/kontakt.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fpages%2Fkontakt.mdx", __ASTRO_IMAGE_IMPORT_1lnYt8]]);
 		
--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,10 @@
 
 export default new Map([
+["src/content/pages/home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fhome.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/impressum.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fimpressum.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/ordinationsraeume.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fordinationsraeume.mdx&astroContentModuleFlag=true")],
-["src/content/pages/home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fhome.mdx&astroContentModuleFlag=true")],
-["src/content/pages/experten/neurologin-dr-violetta-kure.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fexperten%2Fneurologin-dr-violetta-kure.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/kontakt.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fkontakt.mdx&astroContentModuleFlag=true")],
-["src/content/pages/timesharing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Ftimesharing.mdx&astroContentModuleFlag=true")],
-["src/content/pages/experten/kardiologe-dr-peter-huber.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fexperten%2Fkardiologe-dr-peter-huber.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/experten/kardiologe-dr-peter-huber.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fexperten%2Fkardiologe-dr-peter-huber.mdx&astroContentModuleFlag=true")],
+["src/content/pages/experten/neurologin-dr-violetta-kure.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fexperten%2Fneurologin-dr-violetta-kure.mdx&astroContentModuleFlag=true")],
+["src/content/pages/timesharing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Ftimesharing.mdx&astroContentModuleFlag=true")]]);
 		
